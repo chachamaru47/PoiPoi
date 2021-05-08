20942a4fd5b409445c20d9e2b3a0e4f4ea1c45b4
chachamaru47 <45324759+chachamaru47@users.noreply.github.com>
2021-05-09 05:46:50 +0900

    オンライン時のリザルト表示に対応

--- PoiPoi/Assets/Creator Kit - RPG/Scripts/MyUI/Result.cs
+++ PoiPoi/Assets/Creator Kit - RPG/Scripts/MyUI/Result.cs
@@ -10,29 +10,46 @@ namespace RPGM.UI
     /// </summary>
     public class Result : MonoBehaviour
     {
-        public TMPro.TextMeshProUGUI textMeshProUGUIScore;
-        public TMPro.TextMeshProUGUI textMeshProUGUIRecord;
+        public ResultPlayer resultPlayerBase;
+        public RectTransform players;
 
         static Result instance;
+
+        private List<ResultPlayer> resultPlayers;
 
         void Awake()
         {
             instance = this;
+
+            resultPlayers = new List<ResultPlayer>();
+            for (int i = 0; i < Gameplay.NetworkManager.RoomMaxPlayers; i++)
+            {
+                var player = Instantiate(resultPlayerBase);
+                player.transform.SetParent(players);
+                player.Hide();
+                resultPlayers.Add(player);
+            }
             Hide();
+        }
+
+        /// <summary>
+        /// プレイヤー情報を表示する
+        /// </summary>
+        /// <param name="playerGameNo">プレイヤーゲーム番号</param>
+        /// <param name="you">自分である表示をするか</param>
+        /// <param name="winner">勝者表示をするか</param>
+        /// <param name="score">スコア</param>
+        /// <param name="record">レコード</param>
+        public static void ShowPlayer(int playerGameNo, bool you, bool winner, int score, float record)
+        {
+            instance.resultPlayers[playerGameNo].Show(playerGameNo, you, winner, score, record);
         }
 
         /// <summary>
         /// 表示する
         /// </summary>
-        /// <param name="score">得点</param>
-        /// <param name="record">記録</param>
-        public static void Show(int score, float record)
+        public static void Show()
         {
-            // 記録なしは0表示
-            if (record < 0.0f) { record = 0.0f; }
-
-            instance.textMeshProUGUIScore.text = score.ToString();
-            instance.textMeshProUGUIRecord.text = $"{record.ToString("0.00")}m";
             instance.gameObject.SetActive(true);
         }
 
